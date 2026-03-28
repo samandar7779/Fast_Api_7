49656f369fdfb0dd92c461749a9366aa8ff7cb66
Samandar Abdurahimov <hasanovayoqutxon56@gmail.com>
2026-03-28 23:10:42 +0500

    Add static file serving and update uvicorn run

--- 7-dars/main.py
+++ 7-dars/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from typing import Optional
 
 from fastapi import FastAPI, Depends, UploadFile
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.ext.asyncio import AsyncSession
 import uvicorn
 
@@ -94,4 +95,4 @@ async def delete_news(news_id: int, db: AsyncSession = Depends(get_db)):
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(app)
